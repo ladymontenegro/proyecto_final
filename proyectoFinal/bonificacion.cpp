322be92d2425fa79e349d99ac96a854ebade1bbd
Vanessa Montenegro <vanessa.montenegro@udea.edu.co>
2025-07-04 14:10:46 -0500

    Modificacion clase Bonificacion (Animacion de los objetos) - Inclusion de imagenes de las barras de personajes

--- proyectoFinal/bonificacion.cpp
+++ proyectoFinal/bonificacion.cpp
@@ -1,9 +1,34 @@
 #include "bonificacion.h"
-#include <QGraphicsPixmapItem>
+#include <QEasingCurve>
+#include <QTimer>
 
-Bonificacion::Bonificacion(const QPixmap &_sprite, QGraphicsItem *parent): QGraphicsPixmapItem(_sprite, parent) {
-    valorCarga = 1;
+Bonificacion::Bonificacion(const QPixmap &sprite, QGraphicsItem *parent)
+    : QObject(nullptr), QGraphicsPixmapItem(sprite, parent), valorCarga(1)
+{
+    timerAnimacion = new QTimer(this);
+    connect(timerAnimacion, &QTimer::timeout, this, &Bonificacion::actualizarAnimacion);
+    direccionAnimacion = 0.7; // 1 = abajo, -1 = arriba
 }
+
+void Bonificacion::iniciarAnimacionFlotacion() {
+    posicionYInicial = y();
+    timerAnimacion->start(30); // Actualizar cada 50ms
+}
+
+void Bonificacion::detenerAnimacion() {
+    timerAnimacion->stop();
+    setY(posicionYInicial);
+}
+
+void Bonificacion::actualizarAnimacion() {
+    qreal nuevaY = y() + (0.2 * direccionAnimacion);
+    setY(nuevaY);
+
+    // Cambiar dirección si se alcanzan los límites
+    if (y() > posicionYInicial + 5) direccionAnimacion = -1;
+    else if (y() < posicionYInicial - 5) direccionAnimacion = 1;
+}
+
 unsigned short Bonificacion::getValorCarga() const {
     return valorCarga;
 }
--- proyectoFinal/bonificacion.h
+++ proyectoFinal/bonificacion.h
@@ -1,23 +1,27 @@
-
 #ifndef BONIFICACION_H
 #define BONIFICACION_H
 
 #include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QTimer>
 
-class Bonificacion : public QGraphicsPixmapItem
+class Bonificacion : public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
+
 private:
     QPixmap sprite;
+    QTimer *timerAnimacion;
+    qreal posicionYInicial;
+    qreal direccionAnimacion;
     unsigned short valorCarga;
-    unsigned short altoSprite;
-    unsigned short anchoSprite;
-    qreal x;
-    qreal y;
 
 public:
-    Bonificacion(const QPixmap &_sprite, QGraphicsItem *parent = nullptr);
+    explicit Bonificacion(const QPixmap &_sprite, QGraphicsItem *parent = nullptr);
     unsigned short getValorCarga() const;
+    void iniciarAnimacionFlotacion();
+    void detenerAnimacion();
+    void actualizarAnimacion();
 };
 
 #endif // BONIFICACION_H
-
